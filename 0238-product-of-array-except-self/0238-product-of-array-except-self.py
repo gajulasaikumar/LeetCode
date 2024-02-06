1ef0d33844aa7af5e9b0cb2d7afbd7f1a9d70cb7
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-06 09:18:15 +0530

    Time: 154 ms (96.62%), Space: 24.6 MB (47.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,13 +5,11 @@ class Solution:
         for i in range(len(nums)):
             l.append(p)
             p*=nums[i]
-        print(l)
         p1=1
         r=[]
         for i in range(len(nums)-1,-1,-1):
             r.append(p1)
             p1*=nums[i]
-        print(r[::-1])
         r=r[::-1]
         x=[0]*len(nums)
         for i in range(len(l)):
