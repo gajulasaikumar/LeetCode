2486c6b5a49f3e02324bb7e2ea340719f3ae009b
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-07 11:11:09 +0530

    Time: 69 ms (17.42%), Space: 26.8 MB (6.32%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,15 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c=[ [] for i in range(len((s))+1)]
+        d={}
+        for i in s:
+            d[i]=1+d.get(i,0)
+        print(d)
+        for j in d:
+            c[d[j]].append(j)
+        print(c)
+        s1=""
+        for i in c[::-1]:
+            for j in i:
+                s1=s1+j*d[j]
+        return s1
