33c90277d0f8df679abce93fe02e1911114fba4b
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-09 14:38:58 +0530

    Time: 36 ms (60.14%), Space: 16.6 MB (70.50%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        s1=[]
-        t=order
-        for i in s:
-            if i not in t:
-                s1.append(i)
+        d={}
         l=[]
-        for j in range(len(t)):
-            if t[j] in s:
-                l.append(t[j]*s.count(t[j]))
-        return "".join(l+s1)
+        r=""
+        for i in order:
+            if i in s:
+                l.append(i*s.count(i))
+        r="".join(l)
+        for i in s:
+            if i not in order:
+                r=r+i
+        return r
