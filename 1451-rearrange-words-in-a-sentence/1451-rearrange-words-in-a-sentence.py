08508eda1dbe22d50d83e368d51e515796a243d9
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-11 20:18:11 +0530

    Time: 54 ms (31.75%), Space: 19.8 MB (30.16%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -1,14 +1,11 @@
 class Solution:
     def arrangeWords(self, text: str) -> str:
-        d={}
         p=[]
+        m=-1
         text=text.split(" ")
         for i in text:
-            if i.lower() not in d:
-                d[i.lower()]=len(i) 
-        for i in text:
             p.append([len(i),i])
-        m=max(list(d.values()))
+            m=max(m,len(i))
         x=[ [] for i in range(m+1)]
         for k in p:
             x[k[0]].append(k[1])
