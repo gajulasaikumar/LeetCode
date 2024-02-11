f1929d47b42d9a2e54241c3646703dea445134d7
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-11 20:15:49 +0530

    Time: 58 ms (25.93%), Space: 20.7 MB (9.79%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -5,15 +5,13 @@ class Solution:
         text=text.split(" ")
         for i in text:
             if i.lower() not in d:
-                d[i.lower()]=len(i)
-        print(p) 
+                d[i.lower()]=len(i) 
         for i in text:
             p.append([len(i),i])
         m=max(list(d.values()))
         x=[ [] for i in range(m+1)]
         for k in p:
             x[k[0]].append(k[1])
-        print(x)
         l=[]
         for i in x:
             l=l+i
