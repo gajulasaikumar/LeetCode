0e2843e5ab51261cd32ad4a4720faf24a0ff34af
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-15 09:56:11 +0530

    Time: 615 ms (12.39%), Space: 32.9 MB (6.09%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
@@ -6,8 +6,9 @@ class Solution:
         for i in nums[1:]:
             l.append(i+l[-1])
         print(l)
+        print(nums)
         m=-1
-        for i in range(1,len(nums)-1):
+        for i in range(len(nums)-1):
             if l[i]>nums[i+1]:
                 m=l[i+1]
         return m
