95d30052bc8a3f894544bd9d6efa5b2b18808911
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-26 10:23:06 +0530

    Time: 714 ms (30.18%), Space: 31.1 MB (89.05%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -2,17 +2,11 @@ class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
         s=0
         l=[]
-        p=[]
         c=0
-        for i in nums[::-1]:
+        for i in nums:
             s=s+i
             l.append(s)
-        s1=0
-        for i in nums:
-            s1=s1+i
-            p.append(s1)
-        l=l[::-1]
         for i in range(len(nums)-1):
-            if p[i]>=l[i+1]:
+            if l[i]>=l[-1]-l[i]:
                 c+=1
         return c
