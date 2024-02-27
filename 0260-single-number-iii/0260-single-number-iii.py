bd8166d87e05f5eeb8435a0ba842f4cb7773825c
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-27 11:13:53 +0530

    Time: 60 ms (54.32%), Space: 18.7 MB (57.29%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        d={}
+        l=[]
+        for i in nums:
+            d[i]=1+d.get(i,0)
+        for i in d:
+            if d[i]==1:
+                l.append(i)
+        return (l)
+        
