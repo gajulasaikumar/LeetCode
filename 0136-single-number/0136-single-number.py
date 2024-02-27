dbcacc316826da647c301b2e249b60e1bb3046a0
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-02-27 10:31:10 +0530

    Time: 103 ms (91.15%), Space: 19.1 MB (82.85%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a=[]
-        b=[]
+        a=0
         for i in nums:
-            if i not in a:
-                a.append(i)
-            else:
-                b.append(i)
-        for j in a:
-            if j not in b:
-                return j
+            a^=i
+        return a
