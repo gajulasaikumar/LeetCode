3b1cd5ffb1c08172498a1bc5da21930f84bfdfcc
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-03-06 14:57:20 +0530

    Time: 70 ms (5.19%), Space: 16.5 MB (49.56%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -4,7 +4,9 @@ class Solution:
             return str(n)+str(n1)>str(n1)+str(n)
         for i in range(len(nums)):
             for j in range(len(nums)-i-1):
-                if not solve(nums[j],nums[j+1]):
+                if solve(nums[j],nums[j+1]):
+                    continue
+                else:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
         s=""
         for i in nums:
