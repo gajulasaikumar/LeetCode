38859f9aa36bc3e835857c1354d1e37cfe0ff425
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-03-07 21:45:57 +0530

    Time: 61 ms (69.28%), Space: 16.7 MB (21.87%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        c=0
+        for i in range(3,n+1):
+            if i%3==0 or i%7==0 or i%5==0:
+                c=c+i
+        return c
+        
