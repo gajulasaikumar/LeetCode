719d2c2a46a3af8c0d0d6647352a91d3e042d3a9
gajulasaikumar <71595628+gajulasaikumar@users.noreply.github.com>
2024-03-11 14:23:51 +0530

    Time: 56 ms (72.22%), Space: 16.6 MB (98.67%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,23 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        def solve(x):
+            v=x[0]
+            c=1
+            s=""
+            for i in x[1:]:
+                if v[-1]==i:
+                    c=c+1
+                else:
+                    if c>1:
+                        s=s+v[-1]+str(c)
+                    else:
+                        s=s+v[-1]
+                    c=1
+                    v=i
+            if c>1:
+                return (s+v+str(c))
+            return s+v
+        x=solve(chars)
+        print(x)
+        chars[:]=list(x)
+        return len(chars)
